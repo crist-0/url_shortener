f074dd0728f801f0a85fdbf73b6a71096eab131d
Christo <cvt4096@gmail.com>
2026-06-18 16:18:29 +0530

    added cache ttl

--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -4,25 +4,30 @@ import { createShortenLink, getLink } from "../repositories/url.repository.js";
 
 import { nanoid } from "nanoid";
 
-export async function createShortenLinkService(createShortenLinkObj: createShortenLinkDTO) {
+export async function createShortenLinkService(
+  createShortenLinkObj: createShortenLinkDTO,
+) {
+  const shortenId: string = nanoid(7);
 
-    const shortenId: string = nanoid(7);
-
-    const shorten = await createShortenLink(createShortenLinkObj.longUrl, shortenId);
-    return shorten;
+  const shorten = await createShortenLink(
+    createShortenLinkObj.longUrl,
+    shortenId,
+  );
+  return shorten;
 }
 
 export async function getLinkService(shortCode: string) {
+  const link: string | null = await redisClient.get(shortCode);
 
-    const link: string | null = await redisClient.get(shortCode);
+  console.log("link == " + link);
 
-    console.log("link == "+link);
-    
-    if(link == null){
-        const link = await getLink(shortCode);
-        await redisClient.set(shortCode, String(link?.long_url));
-        return link?.long_url;
-    }
+  if (link == null) {
+    const link = await getLink(shortCode);
+    await redisClient.set(shortCode, String(link?.long_url), {
+      EX: 3600,
+    });
+    return link?.long_url;
+  }
 
-    return link;
+  return link;
 }
